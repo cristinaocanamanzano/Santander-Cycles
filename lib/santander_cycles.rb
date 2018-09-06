5c5e5a1b03150b24480122ba42b5bab9b34ce022
Cristina Maria Ocana Manzano <cristinaocana1@gmail.com>
2018-09-06 15:41:01 +0100

    Finished step 13

--- lib/santander_cycles.rb
+++ lib/santander_cycles.rb
@@ -4,21 +4,16 @@ class DockingStation
     attr_reader :bike
 
     def release_bike
-        Bike.new
+      fail "No bikes available" unless @bike
+      @bike
     end
 
     def dock(bike)
+       fail "Sorry, the docking station is full" if @bike 
        @bike = bike
     end
-end
 
-class Bike
-    def working?
-        true 
+    def bike
+      @bike
     end
 end
-
-
-# docking_station = DockingStation.new
-
-# puts docking_station.respond_to? :release_bike
--- spec/santander_cycles_spec.rb
+++ spec/santander_cycles_spec.rb
@@ -3,23 +3,36 @@ require 'santander_cycles'
 describe DockingStation do
     it { is_expected.to respond_to :release_bike }
 
-    it 'expects release_bike to instantiate new Bike class' do
-        bike = Bike.new
-        expect(bike.working?).to eq true
-    end
-
-    
     it { is_expected.to respond_to(:dock).with(1).argument }
 
-    it 'expects user to check if bikes are docked' do
+    #it 'expects user to check if bikes are docked' do
+    describe "#dock" do
+      it "user checks if bikes are docked" do
         bike = Bike.new
         expect(subject.dock(bike)).to eq bike
-    end
+      end
 
-    it 'returns us all of the docked bikes at station' do
+      it 'returns us all of the docked bikes at station' do
         bike = Bike.new
         subject.dock(bike)
         expect(subject.bike).to eq bike
+      end
+
+      it "raises an error when the capacity is full" do
+        subject.dock(Bike.new)
+        expect { subject.dock Bike.new }.to raise_error "Sorry, the docking station is full"
+      end
     end
 
+    describe "#release_bike" do
+      it "releases a bike" do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.release_bike).to eq bike
+      end
+
+      it "raises an error when there are no bikes available" do
+        expect { subject.release_bike }.to raise_error "No bikes available"
+      end
+    end
 end
